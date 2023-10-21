4af91c98338299f74fc7d64f5f7cf968b30a1bc0
daveofderby <daveofderby@gmail.com>
2023-10-21 16:12:26 +0100

    done

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -61,7 +61,10 @@ router.get(
     const campground = await Campground.findById(req.params.id)
       .populate({ path: "reviews", populate: { path: "author" } })
       .populate("author");
-    console.log(!campground.reviews.length);
+    if (!campground.author) {
+      campground.author = "blank";
+    }
+    console.log(campground.author);
     if (!campground) {
       req.flash("error", "Campground not found");
       res.redirect(`/campgrounds`);
