e7e0293a45ab5f1ff3c587e2e4acb9baf63551bd
daveofderby <daveofderby@gmail.com>
2023-10-25 15:16:03 +0100

    Carousel added

--- seeds/index.js
+++ seeds/index.js
@@ -2,11 +2,11 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
+const Review = require("../models/review");
 
-const DB_URL =
-  "mongodb+srv://daveofderby:4tAe9svV1mRKWsCP@cluster0.hm2obzv.mongodb.net/";
+// const DB_URL = "mongodb+srv://daveofderby:4tAe9svV1mRKWsCP@cluster0.hm2obzv.mongodb.net/";
 
-// const DB_URL = "mongodb://localhost:27017/yelp-camp";
+const DB_URL = "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(DB_URL, {
   useNewUrlParser: true,
@@ -24,6 +24,7 @@ const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
 const seedDB = async () => {
   await Campground.deleteMany({});
+  await Review.deleteMany({});
   for (let i = 0; i < 2; i++) {
     const random1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
@@ -31,9 +32,18 @@ const seedDB = async () => {
       author: "652fc1f506016e459d6e6c9a",
       location: `${cities[random1000].city}, ${cities[random1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
-      image: `https://source.unsplash.com/collection/163295`,
       description: `Lorem, ipsum dolor sit amet consectetur adipisicing elit. Cumque necessitatibus quisquam, blanditiis ex totam deleniti nisi sint possimus veniam sit? Minima omnis corporis dignissimos doloribus pariatur culpa dolores ut libero?`,
       price,
+      images: [
+        {
+          url: "https://res.cloudinary.com/dudnzaiuc/image/upload/v1698171754/YelpCamp/jh2mzcxnos4u62fd0skf.jpg",
+          filename: "YelpCamp/jh2mzcxnos4u62fd0skf.jpg",
+        },
+        {
+          url: "https://res.cloudinary.com/dudnzaiuc/image/upload/v1698172647/YelpCamp/wgdllmghhpbwopwqyqgu.jpg",
+          filename: "YelpCamp/wgdllmghhpbwopwqyqgu.jpg",
+        },
+      ],
     });
     await camp.save();
   }
